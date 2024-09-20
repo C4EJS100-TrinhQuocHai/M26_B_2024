f118b0e77a0355b23f645eb5c0ae05f951e0c0cd
trinhquochai <trinhquochai12@gmail.com>
2024-09-20 11:19:39 +0700

    session04 05

---
+++ session04/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <h1>Ref, Reactive, computed, watcher</h1>
+        <p>Ref</p>
+        <p>giá trị biến count : {{ count }}</p>
+        <ul>
+            <li>name: {{ userInfor.name }}</li>
+            <li>address: {{ userInfor.address }}</li>
+        </ul>
+        <button @click="increase">click</button>
+        <button @click="changeInfo">change info</button>
+        <button>giá trị lọc : {{ test(count, 2) }}</button>
+
+        <p>*****************</p>
+        <p>Reactive</p>
+        <p>******************</p>
+        <p>computed</p>
+        <p>giá trị biến count khi X2: {{ numberCount }}</p>
+        <p>giá trị biến count khi bình phương: {{ calulator }}</p>
+        <button @click="handleCLick">handleCLick</button>
+        <p>************************</p>
+        <p>watcher</p>
+        <button @click="handleIncreae">increase</button>
+    </div>
+</template>
+
+<script setup>
+import { ref, reactive, computed, watch } from "vue";
+const count = ref([1, 2, 3, 4]); //{value:0}
+const numberCount = ref(5);
+const number = ref(1);
+const userInfor = ref({
+    id: 1,
+    name: "hồng",
+    address: "Hà Đông",
+});
+console.log("count", count);
+
+const increase = () => {
+    // count.value=count.value + 1
+    // count.value = 5;
+};
+const changeInfo = () => {
+    userInfor.value.address = "Trần Phú";
+    userInfor.value.name = "Thùy Linh";
+};
+
+const test = (arr, id) => {
+    let result = arr.filter((item) => item == id);
+    return result;
+};
+const user = reactive({
+    fullName: " Nguyễn Hồng Nhung",
+    age: 25,
+    address: "Hà Nội",
+});
+const calulator = computed(() => numberCount.value ** 2);
+const handleCLick = () => {
+    numberCount.value *= 2;
+};
+watch(number, (newValue, OldValue) => {
+    // thường đi xử lý call API 
+    console.log("watch được gọi!");
+    console.log("giá trị OldValue", OldValue);
+    console.log("giá trị newValue", newValue);
+});
+const handleIncreae = () => {
+    number.value++;
+};
+</script>
+<style></style>
---
+++ session05/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1>condition & list rendering!</h1>
+        <ul>
+            <li>V-IF</li>
+            <li>V-ELSE</li>
+            <li>V-ELSE-IF</li>
+            <li>V-SHOW</li>
+        </ul>
+        <p>V-IF</p>
+        <p v-if="isCheck">hiển thị v-if nếu điều kiện thỏa mãn!</p>
+        <p v-if="isCheck">hiển thị v-if nếu điều kiện thỏa mãn!</p>
+        <p v-else>hiển thị v-else</p>
+        <p>***********</p>
+        <p>V-SHOW</p>
+        <p v-show="isVShow">
+            element được hiển thị nếu điều kiện v-show thỏa mãn!
+        </p>
+        <p>*************************</p>
+        <p>rendering!</p>
+        <ul>
+            <li v-for="(std, index) in students" :key="std.id">
+                sinh viên số {{ index + 1 }}:{{ std.name }}
+            </li>
+        </ul>
+        <ul>
+            <li v-for="(value, key, index) in useInfo" :key="value.id">
+                {{ key }} - {{ value }} {{ index }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+const isCheck = false;
+const isVShow = false;
+const students = [
+    {
+        id: 1,
+        name: "Phương Thảo",
+    },
+    {
+        id: 2,
+        name: "Minh Đức",
+    },
+    {
+        id: 3,
+        name: "Phi Hùng",
+    },
+];
+const useInfo = {
+    id: 1,
+    name: "Nguyễn Văn A",
+    age: 20,
+    address: "Hà Nội",
+};
+</script>
+<style></style>
